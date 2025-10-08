c8835ebea98f521363c7f7ad83f977b6094c335c
richard8003 <richard.johansson.8003@gmail.com>
2025-10-08 05:52:05 +0200

    fubar

---
+++ boards/shields/fiffel/keypos.h
@@ -0,0 +1,51 @@
+#define POS_ESC 0
+#define POS_Q 1
+#define POS_W 2
+#define POS_F 3
+#define POS_P 4
+#define POS_B 5
+#define POS_J 6
+#define POS_L 7
+#define POS_U 8
+#define POS_Y 9
+#define POS_Ö 10
+#define POS_Å 11
+#define POS_CAPS 12
+#define POS_A 13
+#define POS_R 14
+#define POS_S 15
+#define POS_T 16
+#define POS_G 17
+#define POS_M 18
+#define POS_N 19
+#define POS_E 20
+#define POS_I 21
+#define POS_O 22
+#define POS_Ä 23
+#define POS_L_SHIFT 24
+#define POS_Z 25
+#define POS_X 26
+#define POS_C 27
+#define POS_D 28
+#define POS_V 29
+#define POS_K 30
+#define POS_H 31
+#define POS_COMM 32
+#define POS_DOT 33
+#define POS_SLSH 34
+#define POS_R_SHIFT 35
+#define POS_VT_1 36
+#define POS_VT_2 37
+#define POS_VT_3 38
+#define POS_RT_1 39
+#define POS_RT_2 40
+#define POS_RT_3 41
+
+
+
+
+
+
+
+
+
